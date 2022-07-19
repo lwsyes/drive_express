d7a36d91540a4750b98b7a87c8913cd44ee81789
xiezy <2973912389@qq.com>
2022-07-19 15:50:00 +0800

    push

---
+++ README.md
@@ -0,0 +1,10 @@
+<h2 align="center">网盘的后端服务</h2>
+
+> 技术栈：mysql nodejs express
+
+### How to use
+
+> 1. 下载解压或git clone
+> 2. 安装依赖 npm i
+> 3. 创建数据库，mysql目录下有sql文件，运行sql文件，会创建项目需要的数据库及解构。
+> 4. 修改config.js配置项
--- config.js
+++ config.js
@@ -3,9 +3,9 @@ process.argv[2] && (process.env.DOMAIN = process.argv[2])
 module.exports = {
     // mysql配置信息
     MYSQL_OPTION: {
-        host: '42.193.112.244',
+        host: 'xxx.xx.xxx.xx',
         user: 'root',
-        password: '000616',
+        password: 'xxxxxx',
         port: '3306',
         database: 'net_disk'
     },
@@ -15,20 +15,20 @@ module.exports = {
         port: '465',
         secure: true,
         auth: {
-            user: 'xiezyxyz@qq.com',
-            pass: 'wonywjwhayasdfed'
+            user: 'xxxxxx@qq.com',
+            pass: 'xxxxxxxxxxxx'
         }
     },
     //邮箱模板
     EMAIL_VERIFY_CODE_OPTION: {
-        from: '沐雨凉年<2973912389@qq.com>',
+        from: 'xxxxxx',
         subject: '登录验证码',
         text: '你的验证码是：{verifyCode}，有效期5分钟。',
         html: ''
     },
     //账号异常登录模板
     EMAIL_LOGIN_WARN_OPTION: {
-        from: '沐雨凉年<2973912389@qq.com>',
+        from: 'xxxxxxxxxxxx',
         subject: '登录警告',
         text: '用户：{username}，你的账号在{ipAddress}登录，如果非本人操作，请及时修改密码。',
         html: ''
@@ -36,7 +36,7 @@ module.exports = {
     PORT: 3000, //默认开启端口
     JWTKEY: 'nsodivnjcjksxzfb',//jwt加盐字符串
     WHITE_LIST: ['/api'],//验证黑名单,
-    DOMAIN: process.env.DOMAIN || 'https://drive.xiezy.top',//网站域名
+    DOMAIN: process.env.DOMAIN || 'https://xxxxxx.com',//网站域名
     DEFAULT_AVATAR: 'https://cube.elemecdn.com/e/fd/0fc7d20532fdaf769a25683617711png.png',//用户默认头像
     EVERY_DAY_WORD: 'https://saying.api.azwcl.com/saying/get', //每日一词
     ERROR_LOG: path.resolve(__dirname, 'ErrorLog'),//错误日志地址
